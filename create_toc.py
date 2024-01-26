52b741550e4c4c7d7f3d7258a421f22af57928dd
Christoph Zwerschke <cito@online.de>
2024-01-26 13:34:36 +0000

    Fix directory names and add table of contents

---
+++ create_toc.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+"""Create table of contents with all epics."""
+
+import os
+
+
+def get_toc():
+    """Get table of contents with all epics."""
+    epic_dirs = [d for d in os.listdir(".") if os.path.isdir(d) and d[:1].isdigit()]
+    toc = []
+    for epic_dir in epic_dirs:
+        try:
+            num = int(epic_dir.split("-", 1)[0])
+            if not 0 <= num < 1000:
+                raise ValueError
+        except ValueError:
+            raise RuntimeError(f"Invalid directory name {epic_dir}")
+        spec_path = os.path.join(epic_dir, "technical_specification.md")
+        if not os.path.exists(spec_path):
+            raise RecursionError(f"No technical_specification.md found in {epic_dir}")
+        for line in open(spec_path):
+            if line.startswith("#"):
+                title = line[1:].strip()
+                break
+        else:
+            title = None
+        if not title:
+            raise RuntimeError(f"No title found in {spec_path}")
+        try:
+            desc, name = title.rsplit("(", 1)
+            desc = desc.rstrip()
+            name = name[:-1].strip().capitalize()
+        except ValueError:
+            name = desc = None
+        if not name or not desc:
+            raise RuntimeError(f"Unexpected title in {spec_path}")
+        link = f"[{name}](./{spec_path})"
+        toc.append((num, link, desc))
+    return sorted(toc)
+
+
+def main():
+    """Print table of contents in Markdown format."""
+    for num, link, desc in get_toc():
+        print(f"{num}. {link}: {desc}")
+
+
+if __name__ == "__main__":
+    main()
